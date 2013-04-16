42f33688841673c74d15efd0dc9696d7164fe399
Adomas Jatužis <adomixaszvers@gmail.com>
2013-04-16 10:00:49 +0300

    Aprašiau Katinas konstruktorių

---
+++ klases/katinas.cpp
@@ -0,0 +1,3 @@
+Katinas(int svoris, int gimimo_metai, std::string vardas, int koju_skaicius): Gyvunas(svoris, gimimo_metai, vardas, koju_skaicius) {
+	std::cout << "Miau, aš esu katinas. Mane ką tik sukūrė." << std::endl;
+}
